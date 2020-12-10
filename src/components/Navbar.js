b002ab71a604dd332221a0d419b75c4748899bc1
Victor Ayala <victorayala1996@gmail.com>
2020-12-09 20:00:28 -0500

    Navbar almost done

---
+++ src/components/Navbar.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import {Button} from './Button';
+import './Navbar.css';
+import { IconContext } from 'react-icons/lib';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+    useEffect(() =>{
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton)
+
+    return (    
+        <>
+        <IconContext.Provider value={{color: '#fff'}}>
+        <div className="navbar">
+            <div className="navbar-container container">
+                <Link className="navbar-logo">
+                    Bike Rescue
+                </Link>
+            </div>
+            <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+            </div>
+
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links">
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/about" className="nav-links">
+                        About
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/services" className="nav-links">
+                        Service
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/faq" className="nav-links">
+                        FAQ
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/bikes" className="nav-links">
+                        Bikes
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/contact" className="nav-links">
+                        Contact
+                    </Link>
+                </li>
+                <li className='nav-btn'>
+                {button ? (<Link to="sign-up" className="btn-link">
+                    <Button buttonStyle="btn--outline">Book Now</Button>
+                </Link>):(
+                    <Link to="/sign-up" className="btn-link">
+                        <Button buttonStyle="btn--outline" buttonSize="btn--mobile"></Button>
+                    </Link>)}
+                    </li>
+            </ul>
+        </div>
+        </IconContext.Provider>
+        </>
+    )
+}
+
+export default Navbar
